2b03f79dcc31263361d07d79f308030567c0b906
klaus-moser <60796711+klaus-moser@users.noreply.github.com>
2024-03-07 13:26:36 +0100

    Use requirements file

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 from setuptools import setup, find_packages
-from src.logger.logger import __version__
+from app.logger import __version__
 
 
 # read requirements.txt
-with open('requirements.txt') as f:
+with open('requirements.txt', "r") as f:
     requirements = f.read().splitlines()
+
+# read description
+with open('README.md', "r") as f:
+    long_description = f.read()
 
 
 setup(
     name='logging',
     version=__version__,
-    packages=find_packages(),
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
     author='Klaus Moser',
     author_email='60796711+klaus-moser@users.noreply.github.com',
     description='Packet for a standardized logger.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/klaus-moser/logging',
     install_requires=requirements,
@@ -24,4 +29,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
+    python_requires='>=3.12'
 )
